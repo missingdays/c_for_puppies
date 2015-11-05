1fb9d78fe6f663e8ea6f86fae2ab776e035dfbe6
missingdays <rebovykin@gmail.com>
2015-11-05 20:39:18 +0300

    2.0 add first task solution

---
+++ basic/2_flow_control/tasks/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int k, b;
+	int x, y;
+
+	scanf("%d %d", &k, &b);
+	scanf("%d %d", &x, &y);
+
+	if(y == k*x + b){
+		puts("Да\n");
+	} else {
+		puts("Нет\n")
+	}
+
+}
